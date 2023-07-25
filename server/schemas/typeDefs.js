061d5421300994ec53fafdf3304165554f511dbd
tbohn2 <tanner.bohn@yahoo.com>
2023-07-25 12:10:14 -0700

    Added Auth and Mutation types; edited other types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,25 +2,37 @@ const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
 type User {
+    _id: ID!
     username: String!
     email: String!
+    bookCount: Int!
     password: String!
-    savedBooks: [bookSchema]
+    savedBooks: [Book]
 }
 
-type bookSchema {
+type Book {
+    bookId: ID!
     authors: [String!]
     description: String!
-    bookId: ID!
+    title: String!
     image: String!
     link: String!
-    title: String!
+}
+
+type Auth {
+    token: ID!
+    user: User
 }
 
 type Query {
-    users: [User] 
-    user(_id: String!): User
-    books: [bookSchema] 
+    me(_id: String!): User
+    }
+
+type Mutation {
+    login(email, password): Auth
+    addUser(username, email, password): Auth
+    saveBook([authors], description, title, bookId, image, link): User
+    removeBook(bookId): User
 }
 `
 
